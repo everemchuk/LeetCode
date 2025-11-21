91f48a23711d5a7ea08390859330f765454704da
Yevhen Veremchuk <yevhenveremchuk@Yevhens-MacBook-Pro.local>
2025-11-21 12:28:11 -0800

    update

---
+++ Array/longestCommonPrefix.py
@@ -0,0 +1,43 @@
+"""
+Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+"""
+
+def longestCommonPrefix(strs):
+    # 1. Safety Check: If the list is empty [], return empty string ""
+    if not strs:
+        return ""
+        
+    # 2. Initialization: Assume the first string is the prefix
+    prefix = strs[0]
+    
+    # 3. Loop through the list, starting from the SECOND string (index 1)
+    # We start at 1 because we don't need to compare the first string to itself.
+    for s in strs[1:]:
+        
+        # 4. The 'While' Loop: 
+        # As long as the current string 's' does NOT start with our 'prefix'...
+        while not s.startswith(prefix):
+            
+             # 5. Shorten the prefix
+            # prefix[:-1] means "take the string from start up to the last char"
+            # Effectively, this deletes the last character.
+            prefix = prefix[:-1]
+            
+            # 6. Emergency Exit
+            # If we shortened the prefix so much it became empty, 
+            # it means there is no common prefix at all.
+            if not prefix:
+                return ""
+    
+    # 7. Return whatever is left of the prefix
+    return prefix
+
+test_list = ["flower", "flow", "flight"]
+
+print(longestCommonPrefix(test_list))  # Output: "fl"
+
+
+
--- Array/twoSum.js
+++ Array/twoSum.js
@@ -1,4 +1,12 @@
 
+/*
+Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+*/
+
 // Variant 1
 
 const input = [3, 4, 2, 4, 6]
@@ -8,12 +16,12 @@ function twoSum(nums, target) {
 
 	for (let i = 0; i < nums.length; i++) {
 		for (let j = i + 1; j < nums.length; j++) {
-			 if (nums[i] + nums[j] === target) {
-					return {
-						index: [i, j],
-						value: [nums[i], nums[j]]
-					}
-			 }
+			if (nums[i] + nums[j] === target) {
+				return {
+					index: [i, j],
+					value: [nums[i], nums[j]]
+				}
+			}
 		}
 	}
 
@@ -28,18 +36,18 @@ function TwoSum(nums, target) {
 
 	const map = new Map()
 
-	for(let i = 0; i < nums.length; i++) {
+	for (let i = 0; i < nums.length; i++) {
 
 		let diff = target - nums[i]
-        
-    if (map.has(diff)) {
-    	return {
+
+		if (map.has(diff)) {
+			return {
 				index: [i, map.get(diff)],
 				value: [nums[i], nums[map.get(diff)]]
 			}
-    }
-        
-    map.set(nums[i], i)
+		}
+
+		map.set(nums[i], i)
 
 	}
 
